fb761bab8008bdf3408835527a858f4198873d84
Mael Geoffroy <mael.geoffroy@lydia-app.com>
2023-11-21 22:25:42 +0100

    feat(home): change display

---
+++ src/components/Card.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Box, Card, CardHeader, Heading, Image } from "@chakra-ui/react";
+import { CardType } from "../App";
+
+interface Props {
+  card: CardType;
+}
+
+export const CustomCard: React.FC<Props> = ({ card }) => {
+  return (
+    <Card float="left" w="24%" cursor="pointer" padding="10px">
+      <Box
+        borderRadius="10px"
+        bg="#efefef"
+        backgroundSize="cover"
+        width="fit-content"
+        display="flex"
+        alignItems="center"
+      >
+        <Image
+          src={card.image}
+          mixBlendMode="multiply"
+          borderRadius="10px"
+          w={card.width}
+          h={card.height}
+        />
+      </Box>
+      <Box>
+        <CardHeader
+          color="#000"
+          fontSize="14px"
+          fontFamily="circular"
+          lineHeight="16px"
+          margin={0}
+          marginTop="10px"
+          marginLeft="5px"
+          fontWeight={500}
+        >
+          {card.brand}
+        </CardHeader>
+        <Heading
+          opacity={0.5}
+          marginLeft="5px"
+          marginTop="-2px"
+          fontSize="14px"
+          fontFamily="circular"
+        >
+          {card.price}
+        </Heading>
+      </Box>
+    </Card>
+  );
+};
---
+++ src/constants/cardsList.ts
@@ -0,0 +1,115 @@
+import { CardType } from "../App";
+
+export const cards: CardType[] = [
+  {
+    brand: "Nude project",
+    price: "89€",
+    width: '245px',
+    height: '306.250px',
+    linkToProduct:
+      "https://nude-project.com/collections/hoodies-nude-project/products/basic-patch-capsule-halfzip-black",
+    position: "center",
+    image:
+      "https://nude-project.com/cdn/shop/files/7copia_4000x5000.jpg?v=1693584578",
+  },
+  {
+    brand: "H&M",
+    price: "38€",
+    width: '245px',
+    height: '367.500px',
+    linkToProduct: "https://www2.hm.com/fr_fr/productpage.1024674001.html",
+    position: "center",
+    image:
+      "https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F41%2Ffb%2F41fb1732fb6bdab4756e8f2c902b7e4ed827edb4.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]",
+  },
+  {
+    brand: "H&M",
+    price: "30€",
+    width: '245px',
+    height: '367.500px',
+    linkToProduct: "https://www2.hm.com/fr_fr/productpage.1019679055.html",
+    position: "center",
+    image:
+      "https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F72%2F41%2F72418b81f97e13add394e4e91368ade782068518.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5B%5D%2Ctype%5BLOOKBOOK%5D%2Cres%5Bm%5D%2Chmver%5B1%5D&call=url[file:/product/main]",
+  },
+  {
+    brand: "Burga",
+    price: "38€",
+    width: '245px',
+    height: '245px',
+    linkToProduct:
+      "https://burga.fr/products/mirage-iphone-15-pro-coque?currency=EUR&variant=47325500997967&utm_medium=cpc&utm_source=google&utm_campaign=Google%20Shopping&stkn=013bcef6b881&nbt=nb%3Aadwords%3Ax%3A17895224598%3A%3A&nb_adtype=pla&nb_kwd=&nb_ti=&nb_mi=546555523&nb_pc=online&nb_pi=shopify_FR_8676914135375_47325500997967&nb_ppi=&nb_placement=&nb_li_ms=&nb_lp_ms=&nb_fii=&nb_ap=&nb_mt=&trc_gcmp_id=17895224598&gad_source=1&gclid=EAIaIQobChMIj5jyv6zSggMVEeXtCh3tgwiaEAQYAiABEgLzbfD_BwE&case-type=Tough%20(MagSafe)&couleur-des-d%C3%A9tails-de-la-coque=N%2FA",
+    position: "center",
+    image:
+      "https://d3eif34l41063f.cloudfront.net/prod/694351/2023/11/1700577357.jpg",
+  },
+  {
+    brand: "Sony",
+    price: "429€",
+    width: '245px',
+    height: '245px',
+    linkToProduct:
+      "https://www.cdiscount.com/jeux-pc-video-console/ps5/console-playstation-5-edition-standard/f-1035001-ps5stdchassisc.html#mpos=0|cd",
+    position: "bottom",
+    image:
+      "https://d3eif34l41063f.cloudfront.net/prod/694351/2023/11/1700577468.png",
+  },
+  {
+    brand: "Ikea",
+    price: "299€",
+    width: '245px',
+    height: '245px',
+    linkToProduct:
+      "https://www.ikea.com/fr/fr/p/evedal-lampadaire-marbre-gris-40358592/",
+    position: "center",
+    image:
+      "https://d3eif34l41063f.cloudfront.net/prod/694351/2023/11/1700577500.jpg",
+  },
+  // {
+  //   brand: "Delonghi",
+  //   price: "270€",
+  //   linkToProduct:
+  //     "https://www.cdiscount.com/electromenager/petit-dejeuner-cafe/machine-expresso-automatique-avec-broyeur-delong/f-1101727-bundelonghitasse.html?idOffre=2893247466#cm_sp=PA:12269985:NH:CAR",
+  //   position: "center",
+  //   image:
+  //     "https://www.cafemichel.fr/331/machine-expresso-delonghi-magnifica.jpg",
+  // },
+  // {
+  //   brand: "Toptro",
+  //   price: "150€",
+  //   linkToProduct:
+  //     "https://www.amazon.fr/dp/B0BDK6QTHX/?coliid=I3AY9T7CAV3YVZ&colid=7532RQ6K5TCN&psc=1&ref_=list_c_wl_lv_ov_lig_dp_it",
+  //   position: "center",
+  //   image:
+  //     "https://www.toptro.com/cdn/shop/files/6_1024x1024@2x.jpg?v=1690190074",
+  // },
+  // {
+  //   brand: "Nike",
+  //   price: "120€",
+  //   linkToProduct:
+  //     "https://www.zalando.fr/nike-sportswear-dunk-retro-baskets-basses-whiteblack-ni112o0gn-a11.html?size=43&allophones=0&wmc=SEM330_NB_GO._7455046171_205187277_11471707197.&opc=2211&mpp=google|v1||pla-297612067635||9056511||g|c||56861993277||pla|NI112O0GN-A110095000|297612067635|1|&gclsrc=aw.ds&gad_source=1&gclid=EAIaIQobChMIstKgqLPSggMVWC4GAB1o6gSmEAQYAyABEgILRvD_BwE",
+  //   position: "bottom",
+  //   image:
+  //     "https://img01.ztat.net/article/spp-media-p1/ad8cde9c9d2745f4b714dfc872e64b9a/dbfd82f0b4d146138ca60ccb0ad1fe94.jpg?imwidth=1800",
+  // },
+  {
+    brand: "Nike",
+    price: "130€",
+    width: '245px',
+    height: '245px',
+    linkToProduct:
+      "https://www.courir.com/fr/p/nike-dunk-low-polar-blue-1513978.html",
+    position: "bottom",
+    image:
+      "https://d3eif34l41063f.cloudfront.net/prod/694351/2023/11/1700577586.jpg",
+  },
+  // {
+  //   brand: "Octobre",
+  //   price: "135€",
+  //   linkToProduct:
+  //     "https://www.octobre-editions.com/fr/product/pull-coper/ecru",
+  //   position: "center",
+  //   image:
+  //     "https://media.octobre-editions.com/image/upload/twr7crk9bdz35ln2g149.jpg",
+  // },
+];
